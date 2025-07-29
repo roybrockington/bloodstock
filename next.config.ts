75023696b9b34e2ce231eb23d4f071530a4b1073
roybrockington <r.brockington@gmail.com>
2025-07-29 11:50:46 +0100

    Products mapping

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.sound-service.eu',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.soundservicelabs.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.espguitars-distribution-soundservice.eu',
+      },
+    ],
+  },
+}
 
 export default nextConfig;
