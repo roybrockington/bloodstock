fef0d3a372476b5efe4277e01ad0672b96b659ff
roybrockington <r.brockington@gmail.com>
2025-08-01 11:10:21 +0100

    Merch tab, no Stripe links

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,26 @@
-import products from "./assets/products.json"
-import Image from "next/image"
-import Link from "next/link"
+"use client"
+
+import guitars from "./assets/guitars.json"
+import merch from "./assets/merch.json"
+import { useState } from "react"
+import Products from "./components/Products"
 
 
 export default function Home() {
-    return (
-        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen px-8">
-            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-                <span className="mb-2 h-2 border-b-1 border-b-zinc-300 w-full"></span>
-                {products.map(product =>
-                    <li key={product.sku} className="mb-2 list-none flex flex-col items-center gap-4 pb-4 border-b-1 border-b-zinc-300 w-full">
-                            <Image
-                                className="dark:invert"
-                                src={product.brand}
-                                alt=""
-                                width={80}
-                                height={40}
-                                priority
-                            />
-                        <h3 className="text-xl font-bold text-center">{product.description}</h3>
-                        <div className="flex">
-                            <Image
-                                className="dark:invert"
-                                src={`/images/${product.sku}.jpg`}
-                                alt=""
-                                width={180}
-                                height={38}
-                                priority
-                            />
-                        </div>
-                        <p className="flex gap-4 items-center">
-                            <span className="line-through">£{product.ssp}</span>
-                            <span className="text-xl font-bold">£{product.bloodstock}</span>
-                        </p>
-                        <Link className="py-4 px-8 uppercase bg-gradient-to-r bg-red-700 text-white rounded drop-shadow-lg" href={product.link}>Buy</Link>
-                    </li>
-                )}
 
+    const [tab, setTab] = useState(false)
+
+    return (
+        <div className="font-sans min-h-screen px-8">
+            <aside className="w-full flex list-none gap-4 justify-center text-xl my-4">
+                <li onClick={() => setTab(false)} className={`px-2 rounded-xl cursor-pointer ${!tab ? 'text-white bg-black' : ''}`}>Guitars</li>
+                <li>|</li>
+                <li onClick={() => setTab(true)} className={`px-2 rounded-xl cursor-pointer ${tab ? 'text-white bg-black' : ''}`}>Merch</li>
+            </aside>
+            <main className="flex flex-col [30px] row-start-2 items-center sm:items-start">
+                <span className="mb-2 h-2 border-b-1 border-b-zinc-300 w-full"></span>
+                <Products products={guitars} show={!tab} />
+                <Products products={merch} show={tab} />
             </main>
         </div>
     )
