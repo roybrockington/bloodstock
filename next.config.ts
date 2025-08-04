f561d6b31235759d0fa65ae579aa6b9e8ec3353a
roybrockington <r.brockington@gmail.com>
2025-08-04 14:24:40 +0100

    Added label page

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '*.espguitars-distribution-soundservice.eu',
       },
+      {
+        protocol: 'https',
+        hostname: 'quickchart.io',
+      },
     ],
   },
 }
